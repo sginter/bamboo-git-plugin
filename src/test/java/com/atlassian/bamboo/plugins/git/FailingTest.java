2bfa5b822385c1872865cb4698ad1d5ce408b56d
Slawomir Ginter <sginter@atlassian.com>
2010-12-21 17:52:54 +0100

    Always failing test

---
+++ src/test/java/com/atlassian/bamboo/plugins/git/FailingTest.java
@@ -0,0 +1,13 @@
+package com.atlassian.bamboo.plugins.git;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class FailingTest
+{
+    @Test
+    public void testFailing() throws Exception
+    {
+        Assert.fail("Always fail");
+    }
+}
